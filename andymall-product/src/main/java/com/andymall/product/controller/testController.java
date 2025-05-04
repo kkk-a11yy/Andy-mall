a3675dfd030167e1caaea4af9eb76471c9ce6b95
Ksia <ksiafor@gmail.com>
2025-05-04 14:31:03 +0800

    #dev02 try rebase testController from master

--- andymall-product/src/main/java/com/andymall/product/controller/testController.java
+++ andymall-product/src/main/java/com/andymall/product/controller/testController.java
@@ -4,6 +4,8 @@ public class testController {
     //from dev01
     System.out.printLn("test");
 
+    //change from master after merge dev01
+    // now test dev02 whether gonna rebase with this change
 
     // from dev02
     System.out.printLn("test2");
